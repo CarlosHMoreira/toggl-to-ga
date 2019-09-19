75016840c22ada1eed43e7a6ed332a7545357acb
Carlos Henrique da S. Moreira <kmoreira620@gmail.com>
2019-09-19 01:55:27 -0300

    Adds log to actions in form inputs and creates a userConfirmation in debug mode

--- src/utils/formHelper.ts
+++ src/utils/formHelper.ts
@@ -1,4 +1,5 @@
-import { Page } from 'puppeteer';
+import { Page, ElementHandle } from 'puppeteer';
+import prompts, { PromptObject } from 'prompts';
 
 import l from '../utils/logger';
 
@@ -19,11 +20,36 @@ export const getValueFrom = async (page: Page, selector: string)  => {
     }
 };
 
+export const userConfirmation = async (message = 'Do you confirm ?') => {
+    const promptConfig = {
+        type: 'select',
+        name: 'confirm',
+        message: 'Do you confirm ?',
+        choices: [
+            {
+                title: 'Yep!', 
+                value: 'true',
+            },
+            {
+                title: 'Of course, not!! :(', 
+                value: 'false',
+            },
+        ]
+    } as PromptObject;
+
+    const { confirm } = await prompts(promptConfig);
+    if (confirm === 'false') {
+        throw Error('Action not confirmed');
+    }
+}
+
 
 export const setSelectValue = async (page: Page, selector: string, value: string, shouldWait = false)  => {
     try {
         const querySelector = `.${selector}`;
         const optionQuerySelector = `${querySelector} option[value="${value}"]`
+
+        l.info(`\n About to set select ${selector} with value option ${value} and should ${shouldWait ? '' : 'not'} await`);
 
         await page.waitForSelector(querySelector);
         await page.waitForSelector(optionQuerySelector);
@@ -31,14 +57,12 @@ export const setSelectValue = async (page: Page, selector: string, value: string
             (shouldWait ? page.waitForNavigation() : Promise.resolve()) as Promise<Response|void>,
             page.select(querySelector, value)
         ]);
+    
+        l.d(`${selector} setted value`);
 
-        await page.waitForSelector(querySelector);
-        const elHandle = await page.$(querySelector);
-
-        // if (!elHandle) throw new Error(`Element ${selector} not found`);
-
-        // const jshandle = await elHandle.getProperty('value');
-        // await jshandle.jsonValue();
+        const actualValue = await getValueFrom(page, selector);
+        
+        l.warn(`${selector} now with value ${actualValue}`);
     } catch(error) {
         l.error('Error when triying to set a select option');
         throw error;
@@ -46,18 +70,28 @@ export const setSelectValue = async (page: Page, selector: string, value: string
 };
 
 const typeIt = async (page:Page, selector: string, value: string, clickCount = 3) => {
-    const elHandle =  await page.$(selector);
+    const elHandle  =  await page.$(selector) as ElementHandle<Element>;
     
     if (!elHandle) throw new Error(`Element ${selector} not found`);
-
+    
     await elHandle.click();
+    l.d(`Clicked in ${selector}`);
     await elHandle.focus();
+    l.d(`Focused in ${selector}`);
     await elHandle.click({ clickCount });
+    l.d(`Clicked ${clickCount} times in ${selector}`);
     await elHandle.press('Backspace');
+    l.d(`Pressed Backspace in ${selector}`);
     await elHandle.type(value);
+    l.d(`Typed ${value} in ${selector}`);
+    
+    const jsHandle = await elHandle.getProperty('value');
+    const actualValue = await jsHandle.jsonValue();
+    l.warn(`Actual value is ${actualValue}`);
 };
 
 export const setInputValue = async (page: Page, selector: string, value: string, shouldWait = false)  => {
+    l.info(`\n About to set ${selector} with ${value}, should ${shouldWait ? '' : 'not'} await.`);
     try {
         const querySelector = `.${selector}`;
 
@@ -66,15 +100,6 @@ export const setInputValue = async (page: Page, selector: string, value: string,
             (shouldWait ? page.waitForNavigation() : Promise.resolve()) as Promise<Response|void>,
             typeIt(page, querySelector, value)
         ]);
-
-        // await page.waitForSelector(querySelector);
-        // const elHandle = await page.$(querySelector);
-
-        // if (!elHandle) throw new Error(`Element ${selector} not found`);
-
-        // const jshandle = await elHandle.getProperty('value');
-        // await jshandle.jsonValue();
-
     } catch(error) {
         l.error('Error when triying to set value to');
         throw error;
